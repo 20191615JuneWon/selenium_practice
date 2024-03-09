741c9c05e03776a2d7c21a10c4a1c013c547d7de
20191615JuneWon <71223432+20191615JuneWon@users.noreply.github.com>
2024-03-10 01:22:34 +0900

    Add files via upload

---
+++ pp.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import my_id  # private info
+
+ID = my_id.id
+PASSWORD = my_id.password
+
+driver = webdriver.Chrome()
+url = 'https://ecampus.kookmin.ac.kr/login/index.php'
+driver.get(url)  # bring us to the URL
+
+
+driver.find_element(By.NAME, 'loginId').send_keys(ID)
+
+driver.find_element(By.NAME, 'loginPwd').send_keys(PASSWORD)
+
+driver.find_element(By.CLASS_NAME, 'submitform').click()
+
+driver.find_element(By.TAG_NAME, 'title').screenshot()
+
+
+
+
+
+
+
